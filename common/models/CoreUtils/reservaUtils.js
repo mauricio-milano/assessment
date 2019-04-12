c5c4f2138e62144121182bb6619ac02accbf320a
mauricio-milano <mauricio.milano.santos.da.silva.junior@ibm.com>
2019-04-12 11:44:40 -0300

    organizaçao do codigo

--- common/models/CoreUtils/reservaUtils.js
+++ common/models/CoreUtils/reservaUtils.js
@@ -90,6 +90,73 @@ const verificaDisponibilidade = (ctx, Reserva)=>{
     });
   });
 };
+const sugestaoHorarios = (data, quadras, callback) => {
+  // eslint-disable-next-line no-undef
+  let resultado = [];
+  data.fimEm = new Date(data.fimEm);
+  data.inicioEm = new Date(data.inicioEm);
+
+  let dataUmaHoraAMais = ObjectManipulation.criaObjWhere(
+    data.tipo,
+    ObjectManipulation.alteraHora(data.inicioEm, 1),
+    ObjectManipulation.alteraHora(data.fimEm, 1));
+
+  let dataUmaHoraAMenos = ObjectManipulation.criaObjWhere(
+    data.tipo,
+    ObjectManipulation.alteraHora(data.inicioEm, -1),
+    ObjectManipulation.alteraHora(data.fimEm, -1));
+
+  let outrasQuadras = [
+    ObjectManipulation.criaObjWhere(quadras[0], data.inicioEm, data.fimEm),
+    ObjectManipulation.criaObjWhere(quadras[1], data.inicioEm, data.fimEm)];
+
+  let dataUmaHoraAMaisQuadraDiferente = [
+    ObjectManipulation.criaObjWhere(
+      quadras[0],
+      ObjectManipulation.alteraHora(data.inicioEm, 1),
+      ObjectManipulation.alteraHora(data.fimEm, 1)),
+    ObjectManipulation.criaObjWhere(
+      quadras[1],
+      ObjectManipulation.alteraHora(data.inicioEm, 1),
+      ObjectManipulation.alteraHora(data.fimEm, 1)),
+  ];
+  let dataUmaHoraAMenosQuadraDiferente = [
+    ObjectManipulation.criaObjWhere(
+      quadras[0],
+      ObjectManipulation.alteraHora(data.inicioEm, -1),
+      ObjectManipulation.alteraHora(data.fimEm, -1)),
+    ObjectManipulation.criaObjWhere(
+      quadras[1],
+      ObjectManipulation.alteraHora(data.inicioEm, -1),
+      ObjectManipulation.alteraHora(data.fimEm, -1))];
+  let obj = {
+    dataUmaHoraAMais: dataUmaHoraAMais,
+    dataUmaHoraAMenos: dataUmaHoraAMenos,
+    outrasQuadras1: outrasQuadras[0],
+    outrasQuadras2: outrasQuadras[1],
+    dataUmaHoraAMaisQuadraDiferente1: dataUmaHoraAMaisQuadraDiferente[1],
+    dataUmaHoraAMaisQuadraDiferente2: dataUmaHoraAMaisQuadraDiferente[0],
+    dataUmaHoraAMenosQuadraDiferente1: dataUmaHoraAMenosQuadraDiferente[0],
+    dataUmaHoraAMenosQuadraDiferente2: dataUmaHoraAMenosQuadraDiferente[1],
+  };
+  Object.keys(obj).forEach(key=>{
+    let filtro = object.criaFiltroDeIntervalo(obj[key]);
+    Reserva.find(filtro, (erro, resp)=>{
+      if (erro) {
+        callback(erro);
+      } else {
+        if (resp.length == 0){
+          // eslint-disable-next-line no-undef
+          resultado.push(obj[key]);
+        }
+      }
+    });
+    setTimeout(()=>{
+      // TODO fazer isso ser sincrono e nao por tempo
+      callback(null, resultado);
+    }, 5000);
+  });
+}
 
 module.exports = {
   travaDeHorario,
@@ -98,4 +165,5 @@ module.exports = {
   verificaQuadra,
   verificaDuracao,
   verificaDisponibilidade,
+  sugestaoHorarios,
 };
--- common/models/reserva.js
+++ common/models/reserva.js
@@ -56,52 +56,11 @@ module.exports = function(Reserva) {
       if (resultado.length == 0) {
         callback(null, [data]);
       } else {
-        sugestaoHorarios(data, quadras, callback);
+        reservaModel.sugestaoHorarios(data, quadras, callback);
       }
     });
   };
-  function sugestaoHorarios(data, quadras, callback) {
-    // eslint-disable-next-line no-undef
-    let resultado = [];
-    data.fimEm = new Date(data.fimEm);
-    data.inicioEm = new Date(data.inicioEm);
-    let dataUmaHoraAMais = object.criaObjWhere(data.tipo, object.alteraHora(data.inicioEm, 1), object.alteraHora(data.fimEm, 1));
-    let dataUmaHoraAMenos = object.criaObjWhere(data.tipo, object.alteraHora(data.inicioEm, -1), object.alteraHora(data.fimEm, -1));
-    let outrasQuadras = [object.criaObjWhere(quadras[0], data.inicioEm, data.fimEm), object.criaObjWhere(quadras[1], data.inicioEm, data.fimEm)];
-    let dataUmaHoraAMaisQuadraDiferente = [
-      object.criaObjWhere(quadras[0], object.alteraHora(data.inicioEm, 1), object.alteraHora(data.fimEm, 1)),
-      object.criaObjWhere(quadras[1], object.alteraHora(data.inicioEm, 1), object.alteraHora(data.fimEm, 1))];
-    let dataUmaHoraAMenosQuadraDiferente = [
-      object.criaObjWhere(quadras[0], object.alteraHora(data.inicioEm, -1), object.alteraHora(data.fimEm, -1)),
-      object.criaObjWhere(quadras[1], object.alteraHora(data.inicioEm, -1), object.alteraHora(data.fimEm, -1))];
-    let obj = {
-      dataUmaHoraAMais: dataUmaHoraAMais,
-      dataUmaHoraAMenos: dataUmaHoraAMenos,
-      outrasQuadras1: outrasQuadras[0],
-      outrasQuadras2: outrasQuadras[1],
-      dataUmaHoraAMaisQuadraDiferente1: dataUmaHoraAMaisQuadraDiferente[1],
-      dataUmaHoraAMaisQuadraDiferente2: dataUmaHoraAMaisQuadraDiferente[0],
-      dataUmaHoraAMenosQuadraDiferente1: dataUmaHoraAMenosQuadraDiferente[0],
-      dataUmaHoraAMenosQuadraDiferente2: dataUmaHoraAMenosQuadraDiferente[1],
-    };
-    Object.keys(obj).forEach(key=>{
-      let filtro = object.criaFiltroDeIntervalo(obj[key]);
-      Reserva.find(filtro, (erro, resp)=>{
-        if (erro) {
-          callback(erro);
-        } else {
-          if (resp.length == 0){
-            // eslint-disable-next-line no-undef
-            resultado.push(obj[key]);
-          }
-        }
-      });
-      setTimeout(()=>{
-        // TODO fazer isso ser sincrono e nao por tempo
-        callback(null, resultado);
-      }, 5000);
-    });
-  }
+ 
 
   /**
    * Verifica a disponibilidade
